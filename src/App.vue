baec3f4c8cc93885265f9227d91e33e8a5d1aead
Siu <siuc@protonmail.com>
2022-04-02 14:22:14 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link :to="{ name: 'home' }">Home</router-link> |
     <router-link :to="{ name: 'pets' }">View Pets</router-link>
   </nav>
   <router-view />
