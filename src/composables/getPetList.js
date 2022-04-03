b4d459e2ba5aaa5cec4c0a061a10e64f63448ba5
Siu <siuc@protonmail.com>
2022-04-02 20:00:02 -0400

    getting list

--- src/composables/getPetList.js
+++ src/composables/getPetList.js
@@ -7,13 +7,12 @@ const getPetList = (token) => {
   const loading = ref(false);
   let receiveToken = false;
 
-  const updatePetList = async (pageNum) => {
+  const updatePetList = async () => {
     if (token.value) {
       try {
-        const url = pageNum
-          ? `https://api.petfinder.com/v2/animals?page=${pageNum.value}`
-          : "https://api.petfinder.com/v2/animals";
         loading.value = true;
+        const url = "https://api.petfinder.com/v2/animals?limit=100";
+        // ? `https://api.petfinder.com/v2/animals?limit=100&page=${pageNum.value}`
         const { data } = await axios({
           url,
           method: "get",
@@ -21,7 +20,22 @@ const getPetList = (token) => {
             Authorization: `Bearer ${token.value}`,
           },
         });
-        petList.value = data;
+        petList.value = data.animals.filter((pet) => pet.photos.length);
+        // const {
+        //   pagination: { total_pages: totalPages },
+        // } = data;
+        // console.log(totalPages);
+        // for (let i = 2; i <= totalPages; i++) {
+        //   const url = `https://api.petfinder.com/v2/animals?limit=100&page=${i}`;
+        //   const { data } = await axios({
+        //     url,
+        //     method: "get",
+        //     headers: {
+        //       Authorization: `Bearer ${token.value}`,
+        //     },
+        //   });
+        //   petList.value = petList.value.concat(data.animals);
+        // }
         loading.value = false;
       } catch (e) {
         error.value = e.message;
--- src/views/PetsView.vue
+++ src/views/PetsView.vue
@@ -1,11 +1,8 @@
 <template>
-  <h1>PETS VIEW</h1>
-  <button @click="pageNum++">Next page</button>
-  <div v-for="pet in petList.animals" :key="pet.id">
+  <div v-for="pet in petList" :key="pet.id">
     <div>{{ pet.id }}</div>
     <div>{{ pet.name }}</div>
-    <div>{{ pet.photos }}</div>
-    <img v-if="pet.photos.length" :src="pet.photos[0].small" />
+    <img :src="pet.photos[0].medium" />
   </div>
 </template>
 
@@ -13,14 +10,13 @@
 import getToken from "../composables/getToken";
 import getPetList from "../composables/getPetList";
 import { ref } from "@vue/reactivity";
-import { watch } from '@vue/runtime-core';
+import { watch } from "@vue/runtime-core";
 
 export default {
   setup() {
     const { token, error } = getToken();
     const { petList, updatePetList, loading } = getPetList(token);
     const pageNum = ref(1);
-    watch(pageNum, () => updatePetList(pageNum))
     return { token, error, petList, pageNum };
   },
 };
