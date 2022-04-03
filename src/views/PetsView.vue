52558ddaa20281bf6ce03f0ddae0de66953a62c7
Siu <siuc@protonmail.com>
2022-04-03 16:09:28 -0400

    modify style of the Load more button

--- src/views/PetsView.vue
+++ src/views/PetsView.vue
@@ -2,15 +2,18 @@
   <div class="pets-list">
     <PetCard v-for="pet in petList" :key="pet.id" :pet="pet" />
   </div>
-  <button v-if="!isEndOfPage" @click="loadMore">Load more</button>
+  <div class="load-more" v-if="!isEndOfPage" @click="loadMore">
+    Load more<DownIcon />
+  </div>
 </template>
 
 <script>
 import PetCard from "../components/PetCard.vue";
+import DownIcon from "vue-material-design-icons/MenuDown.vue";
 
 export default {
   props: ["petList", "loadMore", "isEndOfPage"],
-  components: { PetCard },
+  components: { PetCard, DownIcon },
 };
 </script>
 
@@ -19,5 +22,30 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+}
+
+.load-more {
+  background: #e5e4e2;
+  width: 100%;
+  text-align: center;
+  padding: 0.2em 0.5em 0.5em;
+  font-size: 1.2em;
+  border-radius: 15px;
+  font-weight: 600;
+}
+
+.load-more > *:first-child {
+  position: relative;
+  top: 0.4em;
+}
+
+.load-more .material-design-icon {
+  height: 1.5em;
+  width: 1.5em;
+}
+
+.load-more .material-design-icon > .material-design-icon__svg {
+  height: 1.5em;
+  width: 1.5em;
 }
 </style>
