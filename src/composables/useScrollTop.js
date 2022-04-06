d5097564da0a2c10573d376df0c59e997ff1b99c
Siu <siuc@protonmail.com>
2022-04-06 18:17:20 -0400

    Update useScrollTop.js

--- src/composables/useScrollTop.js
+++ src/composables/useScrollTop.js
@@ -3,11 +3,13 @@ import { ref } from "vue";
 const useScrollTop = () => {
   const show = ref(false);
 
+  const backToTop = () => window.scrollTo(0, 0);
+
   const scrollFn = () => {
     const y = document.documentElement.scrollTop;
     show.value = y > 300;
   };
-  return { show, scrollFn };
+  return { show, scrollFn, backToTop };
 };
 
 export default useScrollTop;
