669e3883aea7fe6c8d4225a792549d3b739d8db4
Siu <siuc@protonmail.com>
2022-04-06 17:33:27 -0400

    formatting the back to top button

--- src/components/PetDetails/BackToTop.vue
+++ src/components/PetDetails/BackToTop.vue
@@ -1,15 +1,33 @@
 <template>
-  <div @click="backToTop">TO THE TOP</div>
+  <div @click="backToTop" class="back-to-top">
+    <UpIcon />
+  </div>
 </template>
 
 <script>
+import UpIcon from "vue-material-design-icons/MenuUp.vue";
+
 export default {
   setup() {
     const backToTop = () => window.scrollTo(0, 0);
     return { backToTop };
   },
+  components: { UpIcon },
 };
 </script>
 
 <style>
+.back-to-top {
+  background: #3aab97;
+  color: #fff;
+  border-radius: 50%;
+  height: 5em;
+  width: 5em;
+  position: fixed;
+  right: 2em;
+  bottom: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
