87e7ba625d668c5cf35089a7fe4354a1d4183ca4
Siu <siuc@protonmail.com>
2022-04-10 22:34:11 -0400

    refactor

--- src/components/PetDetails/MediaAlbum/thumbnail.vue
+++ src/components/PetDetails/MediaAlbum/thumbnail.vue
@@ -26,4 +26,55 @@ export default {
 </script>
 
 <style>
+.thumbnails {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.thumbnails > div {
+  width: calc(25% - 0.6em);
+  margin: 0.3em 0.3em;
+  overflow: hidden;
+  position: relative;
+  border-radius: 0.3em;
+  height: 6em;
+}
+
+.thumbnails img:hover {
+  cursor: pointer;
+}
+
+.thumbnails img {
+  min-height: 100%;
+  min-width: 100%;
+}
+
+.image-screen {
+  background: #7d7d7daa;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.video-screen {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.video-screen:hover {
+  cursor: pointer;
+}
+
+.image-screen:hover {
+  cursor: pointer;
+}
 </style>
