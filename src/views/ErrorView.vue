0dbca12757f42f73988f70fc63b1883ba36e8b85
Siu <siuc@protonmail.com>
2022-04-11 16:13:07 -0400

    Update ErrorView.vue

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>ERROR</h1>
+  <div class="error-page">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+  </div>
 </template>
+
+<style>
+.error-page {
+  width: 100%;
+  text-align: center;
+  padding-top: 5em;
+}
+
+.error-page > h1 {
+  font-size: 5em;
+  margin: 0.1em;
+  padding: 0;
+}
+</style>
