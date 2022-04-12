f3de4c1f81ff6b53caf4e8bcaa5115bcf9a235c9
Siu <siuc@protonmail.com>
2022-04-12 00:32:35 -0400

    pass down error

--- src/views/SavedView.vue
+++ src/views/SavedView.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>Liked Pets</h1>
   <div v-if="petList.length">{{ petList }}</div>
   <div v-else>You haven't liked any pet.</div>
 </template>
@@ -7,6 +8,7 @@
 import getLikedPets from "@/composables/getLikedPets";
 
 export default {
+  emits: ["setError"],
   setup(props, { emit }) {
     const petList = getLikedPets(emit);
     return { petList };
